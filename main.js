a1891dd1af996e5d3c1ebcc7756e6bef2eb79af1
sookyung93 <72374908+sookyung93@users.noreply.github.com>
2020-10-14 15:09:12 +0900

    modify JS: 'contact Btn' scroll

--- main.js
+++ main.js
@@ -30,17 +30,9 @@ function createSkillDescription(skills) {
     `;
 }
 
-function scrollToSection(clickTarget) {
-  clickTarget.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link == null) {
-      return;
-    }
-
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
-  });
+function scrollToSection(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
 
 loadSkills()
@@ -62,8 +54,18 @@ document.addEventListener('scroll', () => {
 
 //when clisk the navbar menu, scroll to section
 const navbarMenu = document.querySelector('.navbar__menu');
-scrollToSection(navbarMenu);
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  scrollToSection(link);
+});
 
 //when click the 'contact' btn, scroll to contact section
-const contactBtn = document.querySelector('.home__contact');
-scrollToSection(contactBtn);
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollToSection('#contact');
+});
