6371ee1ae25cd2227ee4867b275df4c2460db98a
sookyung93 <72374908+sookyung93@users.noreply.github.com>
2020-10-15 19:06:30 +0900

    modify scroll to section (show title when scrolling)

--- main.js
+++ main.js
@@ -1,40 +1,5 @@
 'use strict';
 
-function loadSkills() {
-  // 데이터를 받아와서, json 안에 있는 items 반환
-  return fetch('data/skills.json') //
-    .then((response) => response.json())
-    .then((json) => json.skills);
-}
-
-function displaySkills(skills) {
-  const container = document.querySelector('.skillset__left');
-  container.innerHTML = skills
-    .map((skills) => createSkillDescription(skills))
-    .join('');
-}
-
-function createSkillDescription(skills) {
-  return `
-    <div class="skill">
-        <div class="skill__description">
-            <span>${skills.name}</span>
-            <span>${skills.percentage}</span>
-        </div>
-        <div class="skill__bar">
-        <div class="skill__value" style="width: ${parseInt(
-          skills.percentage
-        )}%"></div>
-        </div>
-    </div>
-    `;
-}
-
-function scrollToSection(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-}
-
 loadSkills()
   .then((skills) => {
     displaySkills(skills);
@@ -43,9 +8,9 @@ loadSkills()
 
 //navbar fix
 const navbar = document.querySelector('#navbar');
-const narbarHeight = navbar.getBoundingClientRect().height;
+const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  if (window.scrollY > narbarHeight) {
+  if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
@@ -60,7 +25,6 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
-  navbarMenu.classList.remove('toggle__open');
   scrollToSection(link);
 });
 
@@ -81,6 +45,7 @@ const home = document.querySelector('#home');
 const homeHeight = home.getBoundingClientRect().height;
 const homeContainer = document.querySelector('.home__container');
 document.addEventListener('scroll', () => {
+  navbarMenu.classList.remove('toggle__open');
   const opacity = 1 - window.scrollY / homeHeight;
   homeContainer.style.opacity = opacity;
 });
@@ -134,3 +99,47 @@ workBtnContainer.addEventListener('click', (event) => {
     workContainer.classList.remove('animation-out');
   }, 300);
 });
+
+function loadSkills() {
+  // 데이터를 받아와서, json 안에 있는 items 반환
+  return fetch('data/skills.json') //
+    .then((response) => response.json())
+    .then((json) => json.skills);
+}
+
+function displaySkills(skills) {
+  const container = document.querySelector('.skillset__left');
+  container.innerHTML = skills
+    .map((skills) => createSkillDescription(skills))
+    .join('');
+}
+
+function createSkillDescription(skills) {
+  return `
+    <div class="skill">
+        <div class="skill__description">
+            <span>${skills.name}</span>
+            <span>${skills.percentage}</span>
+        </div>
+        <div class="skill__bar">
+        <div class="skill__value" style="width: ${parseInt(
+          skills.percentage
+        )}%"></div>
+        </div>
+    </div>
+    `;
+}
+
+function scrollToSection(selector) {
+  const scrollTo = document.querySelector(selector);
+  if (selector === '#home' || selector === '#contact') {
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    console.log(`selector:${selector}`);
+    console.log(scrollTo.offsetTop);
+    window.scrollTo({
+      top: scrollTo.offsetTop - navbarHeight,
+      behavior: 'smooth',
+    });
+  }
+}
